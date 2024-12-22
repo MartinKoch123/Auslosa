db63e54e61f503ef7971b09a034869d636860ea9
Fabian Schneider <fabian.schneider90@hornecker.de>
2024-12-22 20:01:20 +0100

    feat: added Animation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,6 +8,8 @@
         private async void OnAuslosaRequested(object sender, EventArgs e) {
 
             AuslosaButton.IsEnabled = false;
+            AuslosaBildle.Source = "auslosa.gif";
+            AuslosaBildle.IsAnimationPlaying = true;
             ThinkingIndicator.IsRunning = true;
             AppAiOutputLabel.Text = "Wart ...";
 
@@ -19,9 +21,17 @@
             AuslosaButton.IsEnabled = true;
             ThinkingIndicator.IsRunning = false;
             if (rnd.Next(0, 2) == 0)
+            {
                 AppAiOutputLabel.Text = "Bisch oba.";
+                AuslosaBildle.IsAnimationPlaying = false;
+                AuslosaBildle.Source = "oba.jpg";
+            }
             else
+            {
                 AppAiOutputLabel.Text = "Bisch unta.";
+                AuslosaBildle.IsAnimationPlaying = false;
+                AuslosaBildle.Source = "unta.jpg";
+            }
         }
     }
 }
